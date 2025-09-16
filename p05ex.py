533e74d6c696806486de346b1d31dad2f92d869a
tau34 <109936561+tau34@users.noreply.github.com>
2025-09-16 21:45:01 +0900

    Fix index for user input in fruit selection
    Adjust input indexing for fruit selection.

--- p05ex.py
+++ p05ex.py
@@ -2,8 +2,9 @@ f = open("fruits.csv", encoding="utf-8")
 l = f.readlines()
 a = ["","",""]
 print("1~10の数字を3つ入力")
-a[0] = l[int(input("1つめ："))]
-a[1] = l[int(input("2つめ："))]
-a[2] = l[int(input("3つめ："))]
+a[0] = l[int(input("1つめ：")) - 1]
+a[1] = l[int(input("2つめ：")) - 1]
+a[2] = l[int(input("3つめ：")) - 1]
 print(a)
+
 f.close()
